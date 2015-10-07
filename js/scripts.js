0668b45352da46d821971ca425e4001d07ad47e7
pol-jiang <moonpeth@gmail.com>
2015-10-07 23:55:50 +0200

    page menu

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,11 @@ function resize(){
 	$(".backgroundImage").css("height", $(window).height()+"px");
 	$(".backgroundImage").css("width", $(window).height()*1.34+"px");
 	$("#button_play").css("margin-top", $(window).height()/2-106+"px");
+	$("#bouton_niveau2").css("margin-top", $(window).height()/2-90+"px");
 }
 
 resize();
 
+$("#storyAudio").bind('ended', function(){
+    window.location.replace("menu.html");
+});   
