a42322067284b98b8bed2dbe8dc64a078a99d3e4
pol-jiang <moonpeth@gmail.com>
2015-10-07 22:47:05 +0200

    constuire les fichiers html

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,8 @@ $(window).resize(function(){
 });
 
 function resize(){
-	$("#home").css("height", $(window).height()+"px");
-	$("#home").css("width", $(window).height()*1.34+"px");
+	$(".backgroundImage").css("height", $(window).height()+"px");
+	$(".backgroundImage").css("width", $(window).height()*1.34+"px");
 	$("#button_play").css("margin-top", $(window).height()/2-106+"px");
 }
 
