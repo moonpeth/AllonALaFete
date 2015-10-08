4254c363f4ed0e326bc709caf6d2aef201776df5
pol-jiang <moonpeth@gmail.com>
2015-10-08 18:25:01 +0200

    add three niveau (brute)

--- js/scripts.js
+++ js/scripts.js
@@ -12,21 +12,25 @@ function resize(){
 	var X = $('.backgroundImage').position().top; 
 	var Y = $('.backgroundImage').position().left;
 	var width = $('.backgroundImage').width();
+	var height = $('.backgroundImage').height();
     //home page
 	$('.divlink').css({'top':X,'left':Y,'width':width});
 	$('#title').css({'width':width});
 	$('#bouton_jouer').css({'width':width*0.22});
 	//menu page
-	$('.menu_buttons').css({'top':X,'left':Y,'width':width, 'margin-top': $(window).height()/2-77+"px"});
+	$('.menu_buttons').css({'top':X,'left':Y,'width':width, 'margin-top': height/2-100+"px"});
     $('.bouton_niveau').css({'width':width*0.15});
-	$('.aide').css({'top':X,'left':Y,'width':width, 'margin-top': $(window).height()/2+40+"px"});
+	$('.aide').css({'top':X,'left':Y,'width':width, 'margin-top': height/2+"px"});
 	$('#bouton_aide').css({'width':width*0.15});
-    
-
+   
 }
 
 resize();
 
 $("#storyAudio").bind('ended', function(){
+    window.location.replace("intro.html");
+});  
+
+$("#paroleAudio").bind('ended', function(){
     window.location.replace("menu.html");
-});   
+}); 
