320ebf530a2a3af7436cfb12b73249bf0728a0b5
pol-jiang <moonpeth@gmail.com>
2015-10-21 17:30:08 +0200

    robe draggable ok

--- js/scripts.js
+++ js/scripts.js
@@ -8,29 +8,38 @@ function resize(){
 	// $("#button_play").css("margin-top", ($(window).height()-124)/2-170+"px");
 	// $("#bouton_niveau2").css("margin-top", $(window).height()/2-90+"px");
 
-	//get the position of backgrand image whose size change with the size of window of each page
-	var X = $('.backgroundImage').position().top; 
-	var Y = $('.backgroundImage').position().left;
-	var width = $('.backgroundImage').width();
-	var height = $('.backgroundImage').height();
-    //home page
-	$('.divlink').css({'top':X,'left':Y,'width':width});
-	$('#title').css({'width':width});
-	$('#bouton_jouer').css({'width':width*0.22});
-	//menu page
-	$('.menu_buttons').css({'top':X,'left':Y,'width':width, 'margin-top': height/2-100+"px"});
-    $('.bouton_niveau').css({'width':width*0.15});
-	$('.aide').css({'top':X,'left':Y,'width':width, 'margin-top': height/2+"px"});
-	$('#bouton_aide').css({'width':width*0.15});
-    //intro page
-    $('#bambi').css({'top':X+height/3,'left':Y+width/3,'width':width*0.4,"height":height*0.5});
-    //niveau page
-    $('#princess').css({'top':X+height/10,'left':Y+width/2,'width':width*0.4,"height":height*0.8});
+	
 
+
+	// //get the position of backgrand image whose size change with the size of window of each page
+	// var X = $('.backgroundImage').position().top; 
+	// var Y = $('.backgroundImage').position().left;
+	// var width = $('.backgroundImage').width();
+	// var height = $('.backgroundImage').height();
+ //    //home page
+	// $('.divlink').css({'top':X,'left':Y,'width':width});
+	// $('#title').css({'width':width});
+	// $('#bouton_jouer').css({'width':width*0.22});
+	// //menu page
+	// $('.menu_buttons').css({'top':X,'left':Y,'width':width, 'margin-top': height/2-100+"px"});
+ //    $('.bouton_niveau').css({'width':width*0.15});
+	// $('.aide').css({'top':X,'left':Y,'width':width, 'margin-top': height/2+"px"});
+	// $('#bouton_aide').css({'width':width*0.15});
+ //    //intro page
+ //    $('#bambi').css({'top':X+height/3,'left':Y+width/3,'width':width*0.4,"height":height*0.5});
+    
+
+
+
+    //niveau page
+    // $('#princess').css({'top':X+height/10,'left':Y+width/2,'width':width*0.4,"height":height*0.8});
+    // $('#robe1').css({'top':X+height/10,'left':Y+width/4,'width':width*0.25,"height":height*0.7});
+   
 }
 
 resize();
 
+/*pour les audios*/
 $("#storyAudio").bind('ended', function(){
     window.location.replace("intro.html");
 });  
@@ -38,3 +47,22 @@ $("#storyAudio").bind('ended', function(){
 $("#paroleAudio").bind('ended', function(){
     window.location.replace("menu.html");
 }); 
+
+/*pour les drag et drop des vetements*/
+function allowDrop(ev)
+{
+	ev.preventDefault();
+}
+
+var robe = null;
+
+function drag(ev)
+{
+	robe = ev.srcElement;
+}
+
+function drop(ev)
+{
+	$(ev.srcElement).html(robe);
+	$(robe).attr("id", "robePlaced");
+}
